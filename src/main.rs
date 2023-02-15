c6086f7368537242b6697883573b9384c0858f51
Albert <albert.rocha@r2t.company>
2023-02-14 22:18:59 -0300

    feature: initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate diesel;
+
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_web::{middleware, web, App, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+mod models;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var(
+        "RUST_LOG",
+        "user-auth-server=debug,actix_web=info,actix_server=info",
+    );
+
+    return Ok(());
+}
