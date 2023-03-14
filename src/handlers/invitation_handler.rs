91f88d747b38a01949394d5313788649934b8f49
Albert <albert.rocha@r2t.company>
2023-03-14 19:40:49 -0300

    feature: get header properties

--- src/handlers/invitation_handler.rs
+++ src/handlers/invitation_handler.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, HttpRequest};
 use diesel::prelude::*;
 use serde::Deserialize;
 
@@ -10,17 +10,19 @@ pub struct InvitationData {
 }
 
 pub async fn post_invitation(
+    req: HttpRequest,
     invitation_data: web::Json<InvitationData>,
     pool: web::Data<Pool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    println!("Chegou");
+    let header = req.headers();
+    let app_id = header.get("x_application_id").expect("Invalid key");
+    println!("{:?}", app_id);
     web::block(move || create_invitation(invitation_data.into_inner().email, pool)).await??;
     return Ok(HttpResponse::Ok().finish());
 }
 
 fn create_invitation(eml: String, pool: web::Data<Pool>) -> Result<(), crate::error_handling::errors::ServiceError> {
     let invitation = dbg!(query(eml, pool)?);
-    println!("{:?}", invitation);
     return Ok(());
 }
 
@@ -31,7 +33,7 @@ fn query(
     use crate::database::schema::invitations::dsl::invitations;
 
     let new_invitation: Invitation = eml.into();
-    let db_connection = &pool.get()?;
+    let db_connection = &mut pool.get()?;
 
     let inserted_invitation = diesel::insert_into(invitations)
         .values(&new_invitation)
